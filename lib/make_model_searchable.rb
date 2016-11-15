b4249b68a3f72b3652cb44fad82680c0c79c1a0f
8parth <parthmodi54@yahoo.com>
2016-11-15 22:53:53 +0530

    bump version - fixed issue in get_valid_fields

--- lib/make_model_searchable.rb
+++ lib/make_model_searchable.rb
@@ -2,6 +2,7 @@ require "make_model_searchable/version"
 require 'active_record'
 
 module MakeModelSearchable
+
 	def searchable_attributes(*options)
     options = options.collect{ |option| option.to_s }
     self.connection
@@ -17,10 +18,21 @@ module MakeModelSearchable
     self.instance_variable_set(:@selected_fields, valid_fields)
   end
 
-  def get_valid_fields(fields)
-    column_names = self.column_names
-    column_types = self.column_types
-    fields.reject { |element| !column_names.include?(element) or column_types[element].type != :string }
+  def get_valid_fields(options)
+    column_names = self.columns.map(&:name)
+    column_types = self.columns.map(&:type)
+    searchable_columns = []
+    if column_names.length == column_types.length
+      column_names.each_with_index do |column_name, index|
+        searchable_columns << column_name if column_types[index].to_sym == :string 
+      end
+    end
+    if searchable_columns.present?
+      options.select{ |option| searchable_columns.include?(option) }
+    else
+      options = []
+    end
+    options
   end
 
   module ClassMethods 
@@ -30,7 +42,6 @@ module MakeModelSearchable
         if search_term
           search_term = "%#{search_term.downcase}%"
           users = self.arel_table
-          # valid_fields = ["first_name", "last_name"]
           arel_node = Arel::Nodes::Node.new
           valid_fields.each_with_index do |val, index|
             if index == 0
@@ -48,30 +59,30 @@ module MakeModelSearchable
       end
     end
 
-    def get_valid_fields(fields)
-      column_names = self.column_names
-      column_types = self.column_types
-      fields.reject { |element| !column_names.include?(element) or column_types[element].type != :string }
-    end
+    # def get_valid_fields(fields)
+    #   column_names = self.columns.map(&:name)
+    #   column_types = self.columns.map(&:type)
+    #   fields.reject { |element| !column_names.include?(element) or column_types[element].type != :string }
+    # end
 
-    def get_join_fields(fields, associations)
-      # self.reflect_on_association(:emails).class_name.constantize.column_names
-      column_names = []
-      associations.each do |association_name|
-        # association_name
-        column_names << self.reflect_on_association(association_name).class_name.constantize.column_names
-      end
-      column_names.flatten!
-      column_names.uniq!
+    # def get_join_fields(fields, associations)
+    #   # self.reflect_on_association(:emails).class_name.constantize.column_names
+    #   column_names = []
+    #   associations.each do |association_name|
+    #     # association_name
+    #     column_names << self.reflect_on_association(association_name).class_name.constantize.column_names
+    #   end
+    #   column_names.flatten!
+    #   column_names.uniq!
 
-      fields.reject { |element| !column_names.include?(element) }
-    end
+    #   fields.reject { |element| !column_names.include?(element) }
+    # end
 
-    def get_valid_associations(associations)
-      associations.collect! { |assoc| assoc.to_sym }
-      association_names = self.reflect_on_all_associations.collect{|assoc| assoc.name  }
-      associations.reject { |element| !association_names.include?(element) }
-    end
+    # def get_valid_associations(associations)
+    #   associations.collect! { |assoc| assoc.to_sym }
+    #   association_names = self.reflect_on_all_associations.collect{|assoc| assoc.name  }
+    #   associations.reject { |element| !association_names.include?(element) }
+    # end
   end
   ActiveRecord::Base.extend MakeModelSearchable
 end
--- lib/make_model_searchable/version.rb
+++ lib/make_model_searchable/version.rb
@@ -1,3 +1,3 @@
 module MakeModelSearchable
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 end
